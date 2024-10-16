0e52d508d8a5e5c61aac44a7b00028f507bb1c86
Jonathan Rodriguez <jonathan@Mac-mini-de-Jonathan.local>
2024-10-16 16:24:45 -0500

    6toCommit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 export default function Header() {
   return (
-    <nav className="main-header navbar navbar-expand navbar-white navbar-light">
+    <nav 
+      className="main-header navbar navbar-expand navbar-white navbar-light" 
+      style={{ height: '56px' }} // Ajusta la altura del header
+    >
       {/* Header vacío */}
     </nav>
   );
